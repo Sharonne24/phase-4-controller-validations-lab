c2e32c30c551523ed0bdb39815915c8885881ecf
Sharon Awuor <sawuor395@gmail.com>
2023-08-20 10:18:58 +0300

    complete

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -6,16 +6,21 @@ class AuthorsController < ApplicationController
     render json: author
   end
 
-  def create
-    author = Author.create(author_params)
+      def create
+        @author = Author.new(author_params)
+    
+        if @author.save
+          redirect_to authors_path, notice: 'Author was successfully created.'
+        else
+          flash.now[:error] = author.errors.full_messages
+          render :new
+        end
+      end
+    
+      private
+    
+      def author_params
+        params.require(:author).permit(:name, :email)
+      end
+    end
 
-    render json: author, status: :created
-  end
-
-  private
-  
-  def author_params
-    params.permit(:email, :name)
-  end
-  
-end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,18 +6,21 @@ class PostsController < ApplicationController
     render json: post
   end
 
-  def update
-    post = Post.find(params[:id])
-
-    post.update(post_params)
-
-    render json: post
-  end
-
-  private
-
-  def post_params
-    params.permit(:category, :content, :title)
-  end
-
-end
+      def update
+        @post = Post.find(params[:id])
+    
+        if @post.update(post_params)
+          redirect_to post_path(@post), notice: 'Post was successfully updated.'
+        else
+          flash.now[:error] = @post.errors.full_messages
+          render :edit
+        end
+      end
+    
+      private
+    
+      def post_params
+        params.require(:post).permit(:title, :category, :content)
+      end
+    end
+    
